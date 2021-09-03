b691d0484c744efc0c7db156d60635596173f231
Noah Fox <nf238220@ohio.edu>
2021-09-03 14:55:45 -0400

    C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ * Noah Fox 9-3-21
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello, git!!" << endl;
+    return 0;
+}// main
+
